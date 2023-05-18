a8ae75520b0177c8b735f2ea127461a97bc7c1c2
NicoFJCruz <nicofjcruz@gmail.com>
2023-05-18 10:22:31 -0300

    Fakedata correction

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('./db')
 const port = process.env.PORT || 3001
 const app = createApp()
 
-db.sync({ force: false }).then(() => {
+db.sync({ force: true }).then(() => {
   console.log('Data Base Connect')
 
   app.listen(port, () => {
